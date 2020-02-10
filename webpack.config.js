b9543859303acf94c456f93f9292aaefbce9c336
Bryce Derriso <b.derriso@gmail.com>
2020-02-10 17:58:29 -0600

    Remap webpack to look for main.js in the correct place.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,9 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/js/main.js',
   output: {
-    filename: 'main.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
 };
