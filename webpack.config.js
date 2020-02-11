74219a1e4b6c753f05c70e9a499060a884989814
Bryce Derriso <b.derriso@gmail.com>
2020-02-10 18:16:37 -0600

    Add css-loader and file-loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,26 @@
-const path = require('path');
+const path = require('path')
 
 module.exports = {
   entry: './src/js/main.js',
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          'file-loader'
+        ]
+      }
+    ]
+  },
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
   },
-};
+}
